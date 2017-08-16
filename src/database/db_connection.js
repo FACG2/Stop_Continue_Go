78f2af5d9fdf41c831a78a778b3585242cd588d2
EngAyoosh <aaa_2008azhar@hotmail.com>
2017-08-16 16:46:10 +0300

    edit db_connection

---
+++ src/database/db_connection.js
@@ -0,0 +1,10 @@
+const {Pool} = require('pg')
+
+require('env2')('./config.env')
+
+if (!process.env.DB_URL) {
+  throw new Error('No DATABASE_URL provided')
+}
+const pool = new Pool({connectionString: process.env.DB_URL})
+
+module.exports = pool
